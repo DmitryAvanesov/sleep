511f5558215a132a8af13852b9f07df4a00381ef
Dima <38003563+DmitryAvanesov@users.noreply.github.com>
2021-08-28 21:06:14 +0700

    Added a bottom menu w/out background highlighting

--- app/src/main/java/com/example/sleep/list/ListActivity.kt
+++ app/src/main/java/com/example/sleep/list/ListActivity.kt
@@ -1,9 +1,10 @@
 package com.example.sleep.list
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.Window
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.sleep.R
@@ -11,11 +12,22 @@ import com.example.sleep.R
 class ListActivity : AppCompatActivity(R.layout.activity_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add<ListFragment>(R.id.list_fragment_container)
+                add<ListFragment>(R.id.list_container)
             }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_home -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
