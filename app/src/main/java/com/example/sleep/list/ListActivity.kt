e71f8dcf72221faa6c7519cd5360fb1f6f3c9add
Dima <38003563+DmitryAvanesov@users.noreply.github.com>
2021-08-28 21:43:26 +0700

    Fixed a menu select listener

--- app/src/main/java/com/example/sleep/list/ListActivity.kt
+++ app/src/main/java/com/example/sleep/list/ListActivity.kt
@@ -1,13 +1,20 @@
 package com.example.sleep.list
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.app.ActivityCompat.invalidateOptionsMenu
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.sleep.R
+import android.view.Menu
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
 
 class ListActivity : AppCompatActivity(R.layout.activity_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +26,32 @@ class ListActivity : AppCompatActivity(R.layout.activity_list) {
                 add<ListFragment>(R.id.list_container)
             }
         }
+
+        setOnItemSelectedListener()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.menu_home -> {
+    private fun setOnItemSelectedListener() {
+        val menu = findViewById<BottomNavigationView>(R.id.menu)
 
-                true
+        menu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    true
+                }
+                R.id.menu_sleep -> {
+                    true
+                }
+                R.id.menu_meditate -> {
+                    true
+                }
+                R.id.menu_music -> {
+                    true
+                }
+                R.id.menu_afsar -> {
+                    true
+                }
+                else -> super.onOptionsItemSelected(it)
             }
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
